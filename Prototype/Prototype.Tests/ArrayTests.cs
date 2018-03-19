dadc1ead193f659da9a3a685ef28a6ad102d39f7
cochsenreither <c.g.ochsenreither@gmail.com>
2018-03-19 10:27:16 -0700

    created soln

---
+++ Prototype/Prototype.Tests/ArrayTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prototype;
+
+namespace Prototype.Tests
+{
+    public static class ArrayTests
+    {
+        public static void ArrayForEach()
+        {
+
+        }
+
+        public static void ArrayForEachWithIndex()
+        {
+
+        }
+    }
+}
