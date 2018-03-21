f2c3ccb239de50e869bd34d3f80a6e64f40a76c9
ochsec <cochsenreither@protonmail.com>
2018-03-20 18:40:50 -0700

    forEach test

---
+++ ScriptKiddie.ConsoleTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScriptKiddie;
+
+namespace ScriptKiddie.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+            array.forEach(item => Console.WriteLine(item));
+
+            List<string> list = new List<string>()
+            {
+                "a", "b", "c"
+            };
+
+            string concat = string.Empty;
+            list.forEach(str =>
+            {
+                concat += str;
+            });
+            Console.WriteLine(concat);
+        }
+    }
+}
---
+++ ScriptKiddie/Array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptKiddie
+{
+    public static class Array
+    {
+        public static void forEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+        {
+            foreach (TSource item in source)
+                action(item);
+        }
+
+        public static void forEach<TSource>(this IEnumerable<TSource> source, Action<TSource, int> action)
+        {
+            int index = 0;
+            foreach (TSource item in source)
+                action(item, index++);
+        }
+    }
+}
